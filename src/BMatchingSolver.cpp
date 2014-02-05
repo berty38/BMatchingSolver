d09258d543138e094b16530df2eda7744e8bde20
CJ Carey <perimosocordiae@gmail.com>
2014-02-05 14:55:59 -0500

    enabling the short option (-B)

--- src/BMatchingSolver.cpp
+++ src/BMatchingSolver.cpp
@@ -158,7 +158,7 @@ int main(int argc, char ** argv) {
 		/* getopt_long stores the option index here. */
 		int option_index = 0;
 
-		c = getopt_long (argc, argv, "w:d:x:n:b:t:i:o:D:Tc:v",
+		c = getopt_long (argc, argv, "w:d:x:n:b:t:i:o:D:Tc:vB",
 				long_options, &option_index);
 
 		/* Detect the end of the options. */
